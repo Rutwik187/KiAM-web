ac7626ef5df7307b136f00eaffbfc5eed7c9dd5d
Rutwik187 <rutwikshinde34@gmail.com>
2023-01-21 21:25:06 +0530

    created navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/images.js
@@ -0,0 +1,5 @@
+import logo from "../assets/logo.svg"
+import call_icon from "../assets/call_icon.svg"
+import mail_icon from "../assets/mail_icon.svg"
+
+export default { logo, call_icon, mail_icon }
