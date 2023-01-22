2b88287cd4e47122e45eb3071113f309b81087eb
Rutwik187 <rutwikshinde34@gmail.com>
2023-01-23 00:32:29 +0530

    added again number counter package

--- src/container/counters/Counters.jsx
+++ src/container/counters/Counters.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./counter.css";
-// import NumberCounter from "number-counter";
+import NumberCounter from "number-counter";
 
 const Counters = () => {
   return (
@@ -8,16 +8,20 @@ const Counters = () => {
       <div className="sub_head-text">Some Numbers</div>
       <div>
         <span>
-          {/* <NumberCounter end={100000} start={85000} delay={7} /> */}
+          <NumberCounter end={100000} start={85000} delay={7} />
         </span>
         <span>Sensors</span>
       </div>
       <div>
-        <span>{/* <NumberCounter end={2000} start={1500} delay={7} /> */}</span>
+        <span>
+          <NumberCounter end={2000} start={1500} delay={7} />
+        </span>
         <span>Sites</span>
       </div>
       <div>
-        <span>{/* <NumberCounter end={20} start={12} delay={7} /> */}</span>
+        <span>
+          <NumberCounter end={20} start={12} delay={7} />
+        </span>
         <span>Projects</span>
       </div>
     </div>
