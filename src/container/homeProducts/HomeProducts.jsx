6d4e94dc58284f15b165d9e08a951efe52f1f8fb
Rutwik187 <rutwikshinde34@gmail.com>
2023-01-25 20:10:44 +0530

    created products section for mobiles

--- src/container/homeProducts/HomeProducts.jsx
+++ src/container/homeProducts/HomeProducts.jsx
@@ -23,7 +23,7 @@ const HomeProducts = () => {
   return (
     <div>
       <Tabs
-        className="home_products app__container"
+        className="home_products-desktop app__container"
         selectedIndex={currentTabIndex}
         onSelect={setCurrentTabIndex}
       >
@@ -60,6 +60,45 @@ const HomeProducts = () => {
           </TabList>
         </div>
       </Tabs>
+
+      <div className="home_products-mobile app__container">
+        <div className="head-text">Products</div>
+        <hr />
+        <div className="p-text">
+          We build read made websites, mobile applications, and elaborate online
+          business services.
+        </div>
+        <Tabs
+          className="home_product-mobile_tab app__container"
+          selectedIndex={currentTabIndex}
+          onSelect={setCurrentTabIndex}
+        >
+          <div className="home_products-list">
+            <TabList className="home_products-tablist">
+              {homeProductsData.map((item) => (
+                <Tab key={item.id}>{item.title}</Tab>
+              ))}
+            </TabList>
+          </div>
+          <div className="home_products_info ">
+            {homeProductsData.map((item, index) => (
+              <TabPanel
+                key={item.id}
+                className={currentTabIndex === index ? "swipe-in" : "swipe-out"}
+              >
+                <div className="home_product-selected-product_info">
+                  <div className="home_product_img mtb-2">
+                    <img src={item.img} alt="access control " />
+                  </div>
+                  <div className="sub_head-text bold-text">{item.title}</div>
+                  <p className="p-text mtb-2">{item.desc}</p>
+                  <p className="arrow-button">Know More -&gt; </p>
+                </div>
+              </TabPanel>
+            ))}
+          </div>
+        </Tabs>
+      </div>
     </div>
   );
 };
