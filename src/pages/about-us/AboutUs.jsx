6347f5c5bd96674c2934884e6851d59de1fac96b
Rutwik187 <rutwikshinde34@gmail.com>
2023-04-13 18:57:49 +0530

    updated to check firebase

--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -27,7 +27,8 @@ const AboutUs = () => {
   return (
     <div>
       <div className="about_us app__container">
-        <div className="head-text">Top Recent Sites</div>
+        <div>.</div>
+        <div className="head-text">Top Recent Sites </div>
         <hr className="hr" />
         <div className="p-text">
           We build readymade websites, mobile applications, and elaborate online
